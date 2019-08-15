03bf5406b65baff90ab32a4f20ce9bbaa133408e
sundapeng <sundapeng@haier.com>
2019-08-15 17:02:49 +0800

    增加表情

--- footer.php
+++ footer.php
@@ -4,8 +4,8 @@
     <div class="layui-col-md12 t-copy">
         <span class="layui-breadcrumb">
             <span>&copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?>.个人博客</a></span>
-            <span class="layui-hide-xs">Poweed by <a href="http://typecho.org/" target="_blank" rel="nofollow">Typecho</a></span>
-            <span>Theme by <a href="https://www.echo.so" target="_blank">Echo</a></span>
+            <span class="layui-hide-xs">Poweed by <a href="https://www.qzroc.com/" target="_blank">Qzroc</a></span>
+            <span>网站备案号：鲁ICP备18036475号-1</span>
         </span>
     </div>
 </div>
