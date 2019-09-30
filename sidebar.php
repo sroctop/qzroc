0df8eab9dff128fae93573b7439c3a8dead169f3
sundapeng <sundapeng@haier.com>
2019-09-30 09:44:34 +0800

    增加友联插件

--- sidebar.php
+++ sidebar.php
@@ -73,4 +73,9 @@
             <a class="layui-btn layui-btn-xs layui-btn-primary" href="https://www.oopsky.top/" title="L&W's Blog" target="_blank">L&W's Blog</a>
         </div>
     </div>
+
+    <div class="link">
+        <h3 class="title-sidebar"><i class="layui-icon">&#xe64c;</i>友情链接<a style="float: right;color: #666;" href="http://wpa.qq.com/msgrd?v=3&uin=756853308&site=qq&menu=yes">申请</a></h3>
+        <?php Links_Plugin::output(); ?>
+    </div>
 </div>
