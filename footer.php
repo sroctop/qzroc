62f46f8082465218d8c70bb971bb4c21ad57ba23
sundapeng <sundapeng@haier.com>
2019-09-30 10:40:04 +0800

    增加友盟统计

--- footer.php
+++ footer.php
@@ -6,11 +6,11 @@
             <span>&copy; <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title(); ?>.个人博客</a></span>
             <span class="layui-hide-xs">Poweed by <a href="https://www.qzroc.com/" target="_blank">Qzroc</a></span>
             <span>网站备案号：鲁ICP备18036475号-1</span>
+            <span><script type="text/javascript" src="https://s4.cnzz.com/z_stat.php?id=1278073340&web_id=1278073340"></script></span>
         </span>
     </div>
 </div>
 
 <?php $this->footer(); ?>
-<script type="text/javascript" src="https://s4.cnzz.com/z_stat.php?id=1278073340&web_id=1278073340"></script>
 </body>
 </html>
