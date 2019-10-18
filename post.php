4599b6f88f5fa42128d3c27b8051deb33e2c74fe
sundapeng <sundapeng@haier.com>
2019-10-18 14:53:18 +0800

    增加打赏

--- post.php
+++ post.php
@@ -52,4 +52,21 @@
     </div>
 </div>
 
+<div style="padding: 10px 0; margin: 20px auto; width: 100%; font-size:16px; text-align: center;">
+    <button id="rewardButton" disable="enable" onclick="var qr = document.getElementById('QR'); if (qr.style.display === 'none') {qr.style.display='block';} else {qr.style.display='none'}">
+        <span>打赏</span></button>
+    <div id="QR" style="display: none;">
+        <div id="wechat" style="display: inline-block">
+            <a class="fancybox" rel="group">
+                <img id="wechat_qr" src="http://qiniu.qzroc.com/Reward/WechatIMG5.png" alt="WeChat Pay"></a>
+            <p>微信打赏</p>
+        </div>
+        <div id="alipay" style="display: inline-block">
+            <a class="fancybox" rel="group">
+                <img id="alipay_qr" src="http://qiniu.qzroc.com/Reward/WechatIMG4.jpeg" alt="Alipay"></a>
+            <p>支付宝打赏</p>
+        </div>
+    </div>
+</div>
+
 <?php $this->need('footer.php'); ?>
